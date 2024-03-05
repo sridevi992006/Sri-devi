b23172a2e2bb8f18aba1bc5e3e925bc0823a883f
sridevi992006 <162297126+sridevi992006@users.noreply.github.com>
2024-03-05 15:59:21 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const display = document.querySelector(".display");
+const outputContainer = document.querySelector(".output");
+const buttons = document.querySelectorAll("button");
+const specialChars = ["%", "*", "/", "-", "+", "="];
+let output = "";
+
+const calculate = (btnValue) => {
+    display.focus();
+    if (btnValue === "=" && output !== "") {
+        output = eval(output.replace("%", "/100"));
+       
+    } else if (btnValue === "AC") {
+        output = "";
+        outputContainer.innerHTML = "";
+    } else if (btnValue === "DEL") {
+        output = output.toString().slice(0, -1);
+    } else {
+        if (output === "" && specialChars.includes(btnValue)) return;
+        output += btnValue;
+    }
+    display.value = output;
+};
+
+buttons.forEach((button) => {
+    button.addEventListener("click", (e) => calculate(e.target.dataset.value));
+});
